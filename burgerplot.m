b5b62b7f1c1b8451cf92546110e2ea98ba9c451d
Ramjee Sharma <64040961+rssw2019@users.noreply.github.com>
2021-05-13 13:06:10 -0400

    Add files via upload

---
+++ burgerplot.m
@@ -0,0 +1,97 @@
+fileID = fopen('fort-1000.100','r');
+formatSpec = '%f %f';
+sizeA = [2 Inf];
+A = fscanf(fileID,formatSpec,sizeA);
+plot(A(1,:),A(2,:))
+fclose(fileID);
+
+hold on
+fileID = fopen('fort-1000.200','r');
+formatSpec = '%f %f';
+sizeA = [2 Inf];
+A = fscanf(fileID,formatSpec,sizeA);
+plot(A(1,:),A(2,:))
+fclose(fileID);
+
+
+hold on
+fileID = fopen('fort-2000.200','r');
+formatSpec = '%f %f';
+sizeA = [2 Inf];
+A = fscanf(fileID,formatSpec,sizeA);
+plot(A(1,:),A(2,:))
+fclose(fileID);
+
+
+hold on
+fileID = fopen('fort-3000.200','r');
+formatSpec = '%f %f';
+sizeA = [2 Inf];
+A = fscanf(fileID,formatSpec,sizeA);
+plot(A(1,:),A(2,:))
+fclose(fileID);
+
+
+hold on
+fileID = fopen('fort-4000.200','r');
+formatSpec = '%f %f';
+sizeA = [2 Inf];
+A = fscanf(fileID,formatSpec,sizeA);
+plot(A(1,:),A(2,:))
+fclose(fileID);
+
+
+hold on
+fileID = fopen('fort-5000.200','r');
+formatSpec = '%f %f';
+sizeA = [2 Inf];
+A = fscanf(fileID,formatSpec,sizeA);
+plot(A(1,:),A(2,:))
+fclose(fileID);
+
+
+hold on
+fileID = fopen('fort-6000.200','r');
+formatSpec = '%f %f';
+sizeA = [2 Inf];
+A = fscanf(fileID,formatSpec,sizeA);
+plot(A(1,:),A(2,:))
+fclose(fileID);
+
+
+hold on
+fileID = fopen('fort-7000.200','r');
+formatSpec = '%f %f';
+sizeA = [2 Inf];
+A = fscanf(fileID,formatSpec,sizeA);
+plot(A(1,:),A(2,:))
+fclose(fileID);
+
+
+hold on
+fileID = fopen('fort-8000.200','r');
+formatSpec = '%f %f';
+sizeA = [2 Inf];
+A = fscanf(fileID,formatSpec,sizeA);
+plot(A(1,:),A(2,:))
+fclose(fileID);
+
+
+hold on
+fileID = fopen('fort-9000.200','r');
+formatSpec = '%f %f';
+sizeA = [2 Inf];
+A = fscanf(fileID,formatSpec,sizeA);
+plot(A(1,:),A(2,:))
+fclose(fileID);
+
+hold on
+
+fileID = fopen('fort-10000.200','r');
+formatSpec = '%f %f';
+sizeA = [2 Inf];
+A = fscanf(fileID,formatSpec,sizeA);
+plot(A(1,:),A(2,:))
+fclose(fileID);
+
+
